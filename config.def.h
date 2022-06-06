6d7065301147210870b6cffe8b720623c476217f
paige <paigevenuto@gmail.com>
2022-06-06 08:27:56 -0400

    initial commit

---
+++ config.def.h
@@ -0,0 +1,35 @@
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char col_chartreuse[]  = "#00ff00";
+static const char col_green1[]      = "#249368";
+
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Monospace:size=18"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { col_green1, col_gray1 },
+	[SchemeSel] = { col_gray4, col_green1 },
+	[SchemeOut] = { col_gray1, col_chartreuse },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 7;
+static unsigned int columns    = 3;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
